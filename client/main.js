bf7bbfe279ed63825ca167f5611e3ef2ba1f6f61
Renganath Chokkalingam <rengnath1234@gmail.com>
2026-03-07 11:18:48 +0530

    feat: Initialize room functionality in frontend

--- client/main.js
+++ client/main.js
@@ -8,3 +8,32 @@ socket.on("connect", () => {
 socket.on("disconnect", () => {
     console.log("Disconnected from server")
 })
+
+socket.on("room_created", (data) => {
+   console.log("Room created:", data.roomCode)
+})
+
+socket.on("room_update", (room_data) => {
+  console.log("players in room...\n", room_data)
+})
+
+document.getElementById("createRoom").onclick = () => {
+  socket.emit("create_room")
+}
+
+document.getElementById("joinRoom").onclick = () => {
+  let code = prompt("Enter room code")
+  if (code === null || code.trim() === "") {
+    alert("Room code cannot be empty")
+    return
+  }
+
+  let name = prompt("Enter your name")
+  if (name === null || name.trim() === "") {
+    alert("Name cannot be empty")
+    return
+  }
+
+  socket.emit("join_room", { roomCode: code.trim(), player_name: name.trim()})
+ 
+}
